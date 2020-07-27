b723c15aecaeff80e69cb1726a0812d0e0188379
Andy Williams <andy@andy.xyz>
2020-07-27 16:14:53 +0100

    Make a cleaner API for the same config format

--- config.go
+++ config.go
@@ -2,21 +2,11 @@ package main
 
 import "image"
 
-type Pages [][] struct {
-	Icon        string `json:"icon,omitempty"`
-	SwitchPage  *int    `json:"switch_page,omitempty"`
-	Text        string `json:"text,omitempty"`
-	Keybind     string `json:"keybind,omitempty"`
-	Command     string `json:"command,omitempty"`
-	Brightness  *int    `json:"brightness,omitempty"`
-	Url 		string	`json:"url,omitempty"`
-	buff image.Image
-}
+type Page []Key
 
 type Config struct {
-	Pages Pages `json:"pages"`
+	Pages []Page `json:"pages"`
 }
-
 
 type Key struct {
 	Icon        string `json:"icon,omitempty"`
